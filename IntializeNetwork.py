bc0ef59cf4ce7702ac5c1027622a239347ef9cce
Zahra Nabila Izdihar <zahranabilaizdihar97@gmail.com>
2019-08-05 17:32:10 +0700

    Add files via upload

---
+++ IntializeNetwork.py
@@ -0,0 +1,16 @@
+from random import seed
+from random import random
+
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
+
+#seed(1)
+network = initialize_network(2, 3, 1)
+for layer in network:
+	print(layer)
