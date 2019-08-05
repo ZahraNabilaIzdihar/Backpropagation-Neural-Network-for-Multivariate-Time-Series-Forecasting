83dc30c05db6214a62888ae003a9d3935c3704cb
Zahra Nabila Izdihar <zahra.informatika@gmail.com>
2019-08-05 17:33:23 +0700

    Update 1.MinMaxNormalization.py

--- 1.MinMaxNormalization.py
+++ 1.MinMaxNormalization.py
@@ -1,10 +1,8 @@
 #import DATASET
 import pandas as pd
 from IPython.display import display
-
 import numpy as np 
 import matplotlib.pyplot as plt
-
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn.metrics import mean_squared_error 
